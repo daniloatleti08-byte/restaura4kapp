2ccd675304b696d5a893d24b2a16c184793c8771
Danilo <daniloatleti08@gmail.com>
2026-05-18 20:23:45 -0300

    feat: adicionar tag do Google Tag Manager (GTM) dinamicamente no frontend

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,32 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Google Tag Manager injection
+const GTM_ID = import.meta.env.VITE_GTM_ID;
+if (GTM_ID) {
+  // Head Script
+  const script = document.createElement('script');
+  script.innerHTML = `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${GTM_ID}');
+  `;
+  document.head.appendChild(script);
+
+  // Noscript Iframe (Body fallback)
+  const noscript = document.createElement('noscript');
+  const iframe = document.createElement('iframe');
+  iframe.src = `https://www.googletagmanager.com/ns.html?id=${GTM_ID}`;
+  iframe.height = '0';
+  iframe.width = '0';
+  iframe.style.display = 'none';
+  iframe.style.visibility = 'hidden';
+  noscript.appendChild(iframe);
+  document.body.insertBefore(noscript, document.body.firstChild);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
